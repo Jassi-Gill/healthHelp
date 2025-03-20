846c437d54aabb8f0b1d672918badfa831bd9dea
Jassi-Gill <jaswindar.gill22b@iiitg.ac.in>
2025-03-20 21:53:16 +0530

    login-jassi

--- healthHelp/base/api/serializers.py
+++ healthHelp/base/api/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework.serializers import ModelSerializer
-from base.models import User
+from base.models import User, Patient, Police, Driver, Hospital
 
-class UsersSerializer(ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
+
+class HospitalSerializer(ModelSerializer):
+    class Meta:
+        model = Hospital
+        fields = "__all__"
+
+class PatientSerializer(ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+
+class DriverSerializer(ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = "__all__"
+
+class PoliceSerializer(ModelSerializer):
+    class Meta:
+        model = Police
         fields = "__all__"
--- healthHelp/base/api/urls.py
+++ healthHelp/base/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.getRoutes),
     path("users/", views.getUsers),
     path('signup', views.signup, name='signup'),
-    # path("rooms/<str:pk>", views.getRoom),
+    path('login/', views.login, name='login'),
 ]
